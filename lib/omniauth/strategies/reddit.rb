5df8a7a50152e73a0f2e82b0dcebf54a9d60c594
Victor Kmita <vkmita@Macintosh-2.local>
2014-02-23 13:35:01 -0800

    Try some crazy shit

--- lib/omniauth/strategies/reddit.rb
+++ lib/omniauth/strategies/reddit.rb
@@ -1,68 +1,35 @@
 require 'omniauth/strategies/oauth2'
 require 'base64'
 require 'rack/utils'
-#require 'oauth2/reddit_client'
 
 module OmniAuth
   module Strategies
     class Reddit < OmniAuth::Strategies::OAuth2
-      AUTHORIZATION_HOST = 'https://ssl.reddit.com'
-      AUTHORIZATION_PATH = '/api/v1/authorize'
-      ACCESS_TOKEN_PATH  = '/api/v1/access_token'
-
-      option :client_options, {
-        site: AUTHORIZATION_HOST,
-        authorize_url: AUTHORIZATION_HOST + AUTHORIZATION_PATH,
-        token_url: AUTHORIZATION_HOST + ACCESS_TOKEN_PATH
-      }
+      #class NoAuthorizationCodeError < StandardError; end
 
       option :name, "reddit"
       option :authorize_options, [:scope, :duration]
 
-      uid { api_v1_me['id'] }
+      option :client_options, {
+        site: 'https://oauth.reddit.com',
+        token_url: 'https://ssl.reddit.com/api/v1/access_token'
+      }
+
+      uid { raw_info['id'] }
 
       info do
         {
-          name: api_v1_me['name'],
-          email: api_v1_me['email']
+          name: raw_info['name'],
+          email: raw_info['email']
         }
       end
 
       extra do
-        {
-          :username => username,
-          :email => email
-          #:subreddits => {
-          #  :subscribed => subreddits_mine_subscriber,
-          #  :moderated => subreddits_mine_moderator
-          #}
-        }
+        {'raw_info' => raw_info}
       end
-      #
-      #def client
-      #  ::OAuth2::RedditClient.new(options.client_id, options.client_secret, deep_symbolize(options.client_options))
-      #end
-
-      def username
-        @username ||= api_v1_me['name']
+      def raw_info
+        @raw_info ||= (access_token.get('/api/v1/me').parsed || {})#.merge(access_token.get('/subreddits/mine/subscriber').parsed || {})
       end
-
-      def email
-        @email ||= api_v1_me['email']
-      end
-
-      def api_v1_me
-        @api_v1_me ||= access_token.get('/api/v1/me').parsed || {}
-      end
-
-      def subreddits_mine_moderator
-        @subreddits_mine_moderator ||= access_token.get('/subreddits/mine/moderator').parsed || {}
-      end
-
-      def subreddits_mine_subscriber
-        @subreddits_mine_subscriber ||= access_token.get('/subreddits/mine/subscriber').parsed || {}
-      end
-
 
       def build_access_token
         options.token_params.merge!(:headers => {'Authorization' => basic_auth_header })
@@ -70,8 +37,21 @@ module OmniAuth
       end
 
       def basic_auth_header
-        "Basic #{Base64.strict_encode64("#{options[:client_id]}:#{options[:client_secret]}")}"
+        "Basic " + Base64.strict_encode64("#{options[:client_id]}:#{options[:client_secret]}")
       end
+
+      MOBILE_USER_AGENTS =  'webos|ipod|iphone|mobile'
+
+      def request_phase
+        options[:client_options].authorize_url = mobile_request? ? 'https://ssl.reddit.com/api/v1/authorize.compact' : 'https://ssl.reddit.com/api/v1/authorize'
+        super
+      end
+
+      def mobile_request?
+        ua = Rack::Request.new(@env).user_agent.to_s
+        ua.downcase =~ Regexp.new(MOBILE_USER_AGENTS)
+      end
+
 
     end
   end
